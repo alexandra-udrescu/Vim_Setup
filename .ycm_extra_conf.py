cef78069d7be7809db1b00a58cd91ec404c9a060
alexandra-udrescu <89578692+alexandra-udrescu@users.noreply.github.com>
2022-08-12 00:29:50 +0300

    Create .ycm_extra_conf.py

---
+++ .ycm_extra_conf.py
@@ -0,0 +1,7 @@
+def Settings( **kwargs ):
+    return {
+      'flags': [ '-x', 'c++', '-Wall', '-Wextra', '-Werror' ],
+    }
+  
+" Put this file in each project ->serach recursively upwards in yerarchy for it
+" -x c for C, -x c++ for C++, -x objc for Objective-C, and -x cuda for CUDA
